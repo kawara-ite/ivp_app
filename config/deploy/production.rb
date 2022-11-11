ba0b1f826df8bd774f1d57047571baaf707955bb
kawara-ite <114914557+kawara-ite@users.noreply.github.com>
2022-11-11 13:09:08 +0900

    修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '13.112.18.149', user: 'ec2-user', roles: %w{app db web}
+server '35.76.60.54', user: 'ec2-user', roles: %w{app db web}
